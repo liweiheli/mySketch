4ba49464b19e3b36561f2785f9c7edcf8357b423
liweiheli <55153621+liweiheli@users.noreply.github.com>
2024-12-18 00:33:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,10 @@
+let potValue = 0; // Value received from Node.js server
 let lightData;
 let index = 0;
-
 let angle = 0;
-// let size = 200;
-// let grow = 1;
-// let inc = 0;
-
-// let shift = 0;
-// let speed = 1;
 
 function preload() {
-  lightData = loadJSON("data.json");
+  lightData = loadJSON("data.json"); // Load static data
 }
 
 function setup() {
@@ -18,81 +12,81 @@ function setup() {
   colorMode(HSB);
   noStroke();
   angleMode(DEGREES);
+
+  // Fetch data from the server every second
+  setInterval(fetchData, 1000);
 }
 
 function draw() {
+  background(255);
 
-  cloudBg();
-
-  //data value
+  // Access light data value
   let lightValue = lightData.lightData[index];
-
   let circleSize = map(lightValue, 440, 615, 500, 600);
-  let circleColor = map(lightValue, 440, 615, 50,0);
-  let sWeight = map(lightValue, 440, 615, 0.1, 1);
+  let circleColor = map(lightValue, 440, 615, 50, 0);
+  let STROKEweight = map(lightValue, 440, 615, 0.1, 1);
   let spinAngle = map(lightValue, 440, 615, 0.5, 1.5);
-  let randomNUmber = map(lightValue, 440, 615, 4.5, 8);
 
-//       for (var y = -200; y <= 600; y+=5) {
-//     push();
-//     noFill();
-//     stroke(0);
-//     fill(0);
-//     rect(0, y + shift, 400, 1);
-//     pop();
-//   }
-//     if (shift >= 200 || shift< 0) {
-//     speed = speed * -1;
-// }
-//   shift = shift + speed;
+  if (potValue > 0.5) {
+    fill(0);
+    rect(0, 0, width, height);
+  } else {
+    cloudBg();
 
-  //animation
-  for (let x = 0; x <= 200; x++) {
-    push();
-    translate(200, 200);
-    rotate(angle);
-    noFill();
-    // stroke(circleColor);
-    strokeWeight(1);
-    rectMode(CENTER);
-    rect(0, 0, 200 - x * 5, 200 - x * 5);
-    pop();
+    for (let x = 0; x <= 200; x++) {
+      push();
+      translate(200, 200);
+      rotate(angle);
+      noFill();
+      strokeWeight(1);
+      rectMode(CENTER);
+      rect(0, 0, 200 - x * 5, 200 - x * 5);
+      pop();
+    }
+
+    for (var x = 0; x <= 400; x++) {
+      push();
+      translate(200, 200);
+      rotate(angle);
+      noFill();
+      stroke(circleColor);
+      strokeWeight(STROKEweight);
+      rectMode(CENTER);
+      rect(0, 0, circleSize - x * 4, circleSize - x * 4);
+      pop();
+    }
+
+    angle += spinAngle;
+    frameRate(5);
+
+    index++;
+    if (index >= lightData.lightData.length) {
+      index = 0;
+    }
   }
-
-  for (var x = 0; x <= 400; x++) {
-    push();
-    translate(200, 200);
-    rotate(angle);
-    noFill();
-    stroke(circleColor);
-    strokeWeight(sWeight);
-    rectMode(CENTER);
-    rect(0, 0, circleSize - x * 4, circleSize - x * 4);
-    pop();
-  }
-
-
-  angle += spinAngle;
-  frameRate(5);
-
-  index++;
-  if (index >= lightData.lightData.length) {
-    index = 0;
-  }
-
 }
 
-//This part's sky and cloud noise effect is referencing to Perlin Noise Clouds made by runemadsen(I initially try to implement an image of cloud background, but doesn't work well)  --> https://editor.p5js.org/runemadsen/sketches/SkpOPexT7
-
-//background
 function cloudBg() {
   background(250);
   for (var x = 0; x < 100; x++) {
     for (var y = 0; y < 100; y++) {
       var r = noise(x / 50, y / 50);
       var col = map(r, 0, 1, 0, 100);
-      fill(220, col, 100,50); // HSB fill color
+      fill(220, col, 100, 50);
       rect(x * 10, y * 10, 10, 10);
     }
   }
+}
+
+// Fetch data from the Node.js server
+function fetchData() {
+  fetch("http://<laptop-ip>:8000/data") // Replace <laptop-ip> with your laptop’s local IP
+    .then((response) => response.json())
+    .then((data) => {
+      potValue = data.value; // Get potValue from server
+      console.log("Fetched potValue:", potValue);
+    })
+    .catch((err) => {
+      console.error("Error fetching data:", err);
+    });
 }
